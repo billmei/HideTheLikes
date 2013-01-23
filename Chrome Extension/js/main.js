f40c49781f4b04f366377198c0d1ccbfa1a3e20c
Bill Mei <info@kortaggio.com>
2013-01-23 02:17:33 -0500

    Set delay so js doesn't choke the CPU

--- Chrome Extension/js/main.js
+++ Chrome Extension/js/main.js
@@ -1,23 +1,69 @@
 (function(){
 
-$(document).bind("DOMSubtreeModified",function(){
-    hideLikes();
-    console.log("updated!")
+
+$(document).ready(function(){
+// Run it once the first time on page load
+    if ($("#timeline_tab_content").length > 0) {
+        hideTimelineLikes();
+    } else if ($("#pagelet_home_stream").length > 0) {
+        hideNewsFeedLikes();
+    } else {
+        console.log("does not exist!")
+    }
+
+// Facebook pulls data in via scripts so we need to re-check if the user has navigated to a new page every second.
+window.setInterval(function(){
+    if ($("#timeline_tab_content").length > 0) {
+        pageListener(hideTimelineLikes,'timeline_tab_content');
+    } else if ($("#pagelet_home_stream").length > 0) {
+        pageListener(hideNewsFeedLikes,'pagelet_home_stream');
+    } else {
+        console.log("does not exist!")
+    }
+},1000);
 });
 
-function hideLikes () {
+function pageListener (pageToHide, idOfDOMNode) {
+    window.HideTheLikes_lastFunctionRun = new Date();
+    window.HideTheLikes_timeRightNow = new Date();
+    window.HideTheLikes_diff = new Date();
+    var delayInterval = 100;
+    // Adds a listener to the timeline to execute the hiding function whenever there is a new post 
+    var timeline = document.getElementById(idOfDOMNode)
+    timeline.addEventListener("DOMNodeInserted",function() {
+        window.HideTheLikes_diff.setTime(window.HideTheLikes_timeRightNow.getTime() - window.HideTheLikes_lastFunctionRun.getTime());
+        if (window.HideTheLikes_diff.getTime() > delayInterval) {
+            pageToHide();
+            window.HideTheLikes_lastFunctionRun = new Date();
+            console.log(window.HideTheLikes_diff.getTime());
+        }
+        window.setTimeout(function(){window.HideTheLikes_timeRightNow = new Date();},delayInterval);
+               
+})
+};
+
+function hideTimelineLikes () {
     $(".UFILikeSentence").remove();
     $(".fbTimelineFeedbackShares").remove();
     $(".fbTimelineFeedbackLikes").remove();
     $(".UFICommentLikeButton").prev().html("");
     $(".UFICommentLikeButton").html("");
-    $(".UFIBlingBoxLikeIcon").next().html("");
-    $(".UFIBlingBoxLikeIcon").remove();
-    $(".UFIBlingBoxReshareIcon")
-    $(".UFIShareLink").parents(".UFIRow").remove();
 
+    console.log("timeline updated!");
+    return;
 }
 
+function hideNewsFeedLikes () {
+    $(".UFILikeSentence").remove();
+    $(".UFICommentLikeButton").prev().html("");
+    $(".UFICommentLikeButton").html("");
+    $(".UFIBlingBoxLikeIcon").parent().remove();
+    $(".UFIBlingBoxReshareIcon").parent().remove();
+    $(".UFIShareLink").parents(".UFIRow").remove();
+
+    console.log("news feed updated!");
+    return;
+};
 
 }());
 
